f42cbe8fc20c8d91efb62a605a3e42be511559fb
Tish Faroul <lfaroul14@gmail.com>
2022-06-23 23:09:42 -0400

    added footer info and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { AiFillGithub, AiOutlineMail, AiFillLinkedin, AiOutlineTwitter, AiOutlineBook } from "react-icons/ai";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer">
+
+      <a href='https://github.com/LatishaFar1'>
+        <AiFillGithub />
+      </a>
+
+      <a href='lfaroul14@gmail.com'>
+        <AiOutlineMail />
+      </a>
+
+
+      <a href='https://www.linkedin.com/in/latisha-faroul/'>
+        <AiFillLinkedin />
+      </a>
+
+
+      <a href='https://twitter.com/TishFaroul'>
+        <AiOutlineTwitter />
+      </a>
+
+
+      <a href='https://tishfaroul.medium.com/'>
+        <AiOutlineBook />
+      </a>
+ 
+    </div>
   )
 }
 
